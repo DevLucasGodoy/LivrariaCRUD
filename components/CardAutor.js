c2c4f592034b62d9103e597b96a8a7ffc13ffe34
Lucas Godoy <54687280+DevLucasGodoy@users.noreply.github.com>
2022-07-01 20:09:28 +0000

    Update put

--- components/CardAutor.js
+++ components/CardAutor.js
@@ -11,27 +11,57 @@ export default function ListagemAutor (props) {
         alert("Excluído com sucesso.")       
       } 
     }
-  async function AlterarAutor() {
-    let alterar = confirm("Você tem certeza?")
-      if (alterar == true) {
-            const response = await api.update("/autores/" + props.id)
-            console.log(response);
-            alert("Alterado com sucesso.")       
-      }
+
+export default function AtualizaAutor (props) {
+  const [autores, setAutores] = useState({
+    nome: props.nome,
+    sobrenome: props.sobrenome,
+    data_nascimento: props.data_nascimento,
+  });
+  let router = useRouter();
+  
+const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setAutores({ ...autores, [id]: value });
+  };
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+    nome: autores.nome,
+    sobrenome: autores.sobrenome, 
+    data_nascimento: autores.data_nascimento,
     }
+    console.log(data);
+    console.log(props.id);
+    const response = await axios.put(`https://livrariacrud-api.lucasgodoy04.repl.co/autores/${props.id}`, data)
+     if (!response.statusText === "OK") {
+      alert("Erro");
+    } else {
+      router.push('/pages/listaAutores')
+    }
+    console.log(response)
+  }
   
   return(
     <div className={styles.card}>
      <b> 
        <div className={styles.autores}>
          <h3>
-           <Link href={`/autor/${props.id}`}><a><br/>{props.nome} <br/>{props.sobrenome}</a></Link>
+           <Link href={`/autor/${props.id}`}>
+             <a>
+             <br/> {props.nome} 
+             <br/> {props.sobrenome}
+             </a>
+           </Link>
          </h3> 
-            Id: {props.id} <br/> Data de nascimento: {props.data_nascimento} 
+             <br/> Id: {props.id} 
+             <br/> Data de nascimento: {props.data_nascimento} 
        </div> 
      </b>
      
       <button className={styles.deletar} onClick={DeletarAutor}>Deletar</button>
+      <button className={styles.deletar} onClick={AlterarAutor}>Alterar</button>
     </div>
   )
 }
--- components/CardLivro.js
+++ components/CardLivro.js
@@ -11,19 +11,61 @@ export default function ListagemAutor (props) {
       }
     }
 
-    async function AlterarLivro() {
-    let alterar = confirm("Você tem certeza?")
-      if (alterar == true) {
-            const response = await api.update("/livros/" + props.id)
-            console.log(response);
-            alert("Alterado com sucesso.")       
-      }
+export default function AlterarLivro (props) {
+  const [autores, setAutores]= useState([])
+  useEffect( async ()=>{
+    api.get('/autors')
+      .then(response =>{ setAutores(response.data) })
+      .catch(err => { console.log("Erro", err) })
+  }, [])
+  console.log(autores);
+  
+const [livros, setLivros] = useState({
+    titulo: props.titulo,
+    editora: props.editora,
+    autorId: props.autorId,
+    data_publicacao: props.data_publicacao,
+    preco: props.preco
+  });
+  let router = useRouter();
+
+const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setLivros({ ...livros, [id]: value });
+  };
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = {
+    titulo: livros.titulo,
+    editora: livros.editora,
+    autorId: livros.autorId,
+    data_publicacao: livros.data_publicacao,
+    preco: livros.preco
     }
+    console.log(data);
+    const response = await axios.put(`https://livrariacrud-api.lucasgodoy04.repl.co/livros/${props.id}`, data)
+   if (!response.statusText === "OK") {
+      alert("Erro");
+    } else {
+      router.push('/pages/listaLivros')
+    }
+    console.log(response);
+  }  
+  
   return(
     <div className={styles.card}>
-    <b>  <div className={styles.livros}><h3> {props.titulo} </h3><br/>Editora: {props.editora} <br/> Preço: {props.preco} <br/> Autor: {props.autorId} <br/> Data de Publicação: {props.data_publicacao}</div> </b>
+    <b>
+      <div className={styles.livros}>
+        <h3> {props.titulo} </h3>
+        <br/> Editora: {props.editora} 
+        <br/> Preço: {props.preco} 
+        <br/> Autor: {props.autorId} 
+        <br/> Data de Publicação: {props.data_publicacao}
+      </div> 
+    </b>
       <button className={styles.deletar} onClick={DeletarLivro}>Deletar</button>
-      <button className={styles.deletar} onClick={DeletarLivro}>Alterar</button>
+      <button className={styles.deletar} onClick={AlterarLivro}>Alterar</button>
     </div>
   )
 }
